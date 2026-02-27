5628aa8f4d7a4a3039fa95e6df25937ac56d4268
aylinndz <154611196+aylinndz@users.noreply.github.com>
2026-02-27 17:19:45 +0300

    Add files via upload

---
+++ myFirstLabScript.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+cout << "whats your name?" << endl;
+cin>> name;
+cout <<"name"<< name << endl;
+
+    cout << "hats your id?" << endl;
+    cin >> id;
+    cout <<"id: : "<< id << endl;
+return 0;
+
+}
